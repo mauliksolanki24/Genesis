474d56adc83e9949ecd325122ea12edb529b302a
mauliksolanki24 <9925428381maulik@gmail.com>
2024-07-26 22:16:11 -0700

    Add files via upload

---
+++ ERT/script.js
@@ -0,0 +1,18 @@
+
+
+// let minibox =document.querySelector('.mini-box')
+// let upscroll = window.pageYOffset;
+
+// window.onscroll = function() {
+
+//     let gapscroll = window.pageYOffset;
+
+//     if (upscroll>gapscroll) {
+//          minibox.style.display="flex"
+//     }
+//     else {
+//         minibox.style.display="none"
+//     }
+    
+//     minibox=upscroll
+// }
